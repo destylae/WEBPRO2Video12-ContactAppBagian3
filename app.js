20ed88664ea1cd3e84002f9de49900f2b12d04d8
destylae <destypati1122@gmail.com>
2024-05-23 11:50:36 +0700

    Contact App - Bagian 2

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
-const contacts = require('./contacts')
+const yargs = require("yargs");
+const contacts = require("./contacts");
 
-const main = async ( ) => {
-    const nama = await contacts.tulisPertanyaan('Masukkan nama Anda : ');
-    const email = await contacts.tulisPertanyaan('Masukkan email Anda : ');
-    const noHP = await contacts.tulisPertanyaan('Masukkan No HP Anda : ');
-    
-    contacts.simpanContact(nama, email, noHP);
-};
+yargs.command({
+    command: 'add',
+    describe: 'Menambahkan contact baru',
+    builder: {
+       nama: {
+        describe: 'Nama Lengkap',
+        demandOption: true,
+        type: 'string',
+       },
+       email: {
+        describe: 'Email',
+        demandOption: false,
+        type: 'string',  
+       },
+       noHP: {
+        describe: 'Nomor Handphone',
+        demandOption: true,
+        type: 'string',  
+       },
+    },
+    handler(argv) {
+        contacts.simpanContact(argv.nama, argv.email, argv.noHP);
+        // const contact = {
+        //     nama: argv.nama,
+        //     email: argv.email,
+        //     noHP: argv.noHP,
+        // };
+        // console.log(contact);
+    },
+    // 'add',
+    // 'Menambahkan contact baru',
+    // () => {},
+    // (argv) => {
+    //     console.log(argv.nama);
+    });
 
-main();
+yargs.parse();
